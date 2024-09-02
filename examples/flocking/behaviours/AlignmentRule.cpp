35712bf02294a9e5af486d2c16f8980dcac85440
ryanboehm123 <ryan.boehm@mymail.champlain.edu>
2024-09-02 11:32:46 -0400

    Alignment rule implemented

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -7,6 +7,12 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
 
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
+  for(const Boid* b : neighborhood) {
+    averageVelocity += {b->getPosition().x, boid->getPosition().y};
+  }
+  if(neighborhood.size() > 1) {
+    averageVelocity /= neighborhood.size();
+  }
 
   return Vector2f::normalized(averageVelocity);
 }
--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -2,10 +2,11 @@
 #include "../gameobjects/Boid.h"
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
-  Vector2f cohesionForce;
+  Vector2f cohesionForce = Vector2f::zero();
 
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
+
 
   // find center of mass
 
