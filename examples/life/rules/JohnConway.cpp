8aceaf8f5c328f7afdede0ff836a288e555f8252
ryanboehm123 <ryan.boehm@mymail.champlain.edu>
2024-09-09 11:07:57 -0400

    John Conway's verison implemented

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -7,12 +7,20 @@ void JohnConway::Step(World& world) {
     for(int j = 0; j < world.SideSize(); j++) {
       Point2D pointToCalculate = {i,j};
       if(world.Get(pointToCalculate)) {
-        if(CountNeighbors(world, pointToCalculate) < 2 || CountNeighbors(world, pointToCalculate) > 3) {
+        if(CountNeighbors(world, pointToCalculate) < 2) {
+          world.SetNext(pointToCalculate, false);
+        } else if (CountNeighbors(world, pointToCalculate) == 2 || CountNeighbors(world, pointToCalculate) == 3) {
+          world.SetNext(pointToCalculate, true);
+        } else {
           world.SetNext(pointToCalculate, false);
         }
       }
-      else if (CountNeighbors(world, pointToCalculate) == 3) {
-        world.SetNext(pointToCalculate, true);
+      else {
+        if (CountNeighbors(world, pointToCalculate) == 3) {
+          world.SetNext(pointToCalculate, true);
+        } else {
+          world.SetNext(pointToCalculate, false);
+        }
       }
     }
   }
@@ -23,8 +31,10 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   int count = 0;
   for(int i = -1; i <= 1; i++) {
     for(int j = -1; j <= 1; j++) {
-      Point2D pointNeighbor = point + Point2D(i, j);
-      count += world.Get(pointNeighbor);
+      Point2D pointNeighbor = {point.x + i, point.y + j};
+      if(world.Get(pointNeighbor) && (i != 0 || j != 0)) {
+       count++;
+      }
     }
   }
   return count;
